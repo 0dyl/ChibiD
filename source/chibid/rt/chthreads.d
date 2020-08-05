4beb23ad753f291172a99f948771f55e1a21c682
Dylan Graham <dylan.graham2000@outlook.com>
2020-08-05 14:11:44 +1000

    chthreads

---
+++ source/chibid/rt/chthreads.d
@@ -0,0 +1,64 @@
+module chibid.rt.chthreads;
+
+import chibid.port;
+import chibid.rt.chtime;
+
+struct thread_descriptor_t
+{
+    const char* name;
+    stkalign_t* wbase;
+    stkalign_t* wend;
+    tprio_t prio;
+    tfunc_t funcp;
+    void* arg;
+}
+
+auto THD_WORKING_AREA_SIZE(auto n) { 
+    return MEM_ALIGN_TEXT(thread_t.sizeof + PORT_WA_SIZE(n), PORT_STACK_ALIGN); }
+auto THD_WORKING_AREA(auto s, auto n) { return PORT_WORKING_AREA(s, n); }
+auto THD_WORKING_AREA_BASE(auto s) { return cast(stkalign_t*)s;}
+auto THD_WORKING_AREA_END(auto s) { return WORKING_AREA_BASE(s) + s.sizeof / stkalign.sizeof; }
+auto THD_FUNCTION(auto tname, auto arg) { return PORT_THD_FUNCTION(tname, arg); }
+
+void chThdSleepSeconds(time_secs_t sec) { chThdSleep(TIME_S2I(sec)); }
+void chThdSleepMilliseconds(time_msecs_t msec) { chThdSleep(TIME_MS2I(msec)); }
+void chThdSleepMicroseconds(time_usecs_t usec) { chThdSleep(TIME_US2I(usec)); }
+
+alias tfunc_t = void function(void* p);
+
+thread_t* chThdCreateSuspendedI(const thread_descriptor_t* tdp);
+thread_t* chThdCreateSuspended(const thread_descriptor_t* tdp);
+thread_t* chThdCreateI(const thread_descriptor_t *tdp);
+thread_t* chThdCreate(const thread_descriptor_t *tdp);
+thread_t* chThdCreateStatic(void *wsp, size_t size, tprio_t prio, tfunc_t pf, void *arg);
+thread_t* chThdStart(thread_t *tp);
+thread_t* chThdAddRef(thread_t *tp);
+void chThdRelease(thread_t *tp);
+void chThdExit(msg_t msg);
+void chThdExitS(msg_t msg);
+msg_t chThdWait(thread_t *tp);
+tprio_t	chThdSetPriority(tprio_t newprio);
+void chThdTerminate(thread_t *tp);
+msg_t chThdSuspendS(thread_reference_t *trp);
+msg_t chThdSuspendTimeoutS(thread_reference_t *trp, sysinterval_t timeout);
+void chThdResumeI(thread_reference_t *trp, msg_t msg);
+void chThdResumeS(thread_reference_t *trp, msg_t msg);
+void chThdResume(thread_reference_t *trp, msg_t msg);
+msg_t chThdEnqueueTimeoutS(threads_queue_t *tqp, sysinterval_t timeout);
+void chThdDequeueNextI(threads_queue_t *tqp, msg_t msg);
+void chThdDequeueAllI(threads_queue_t *tqp, msg_t msg);
+void chThdSleep(sysinterval_t time);
+void chThdSleepUntil(systime_t time);
+systime_t chThdSleepUntilWindowed(systime_t prev, systime_t next);
+void chThdYield();
+thread_t* chThdGetSelfX();
+tprio_t chThdGetPriorityX();
+systime_t chThdGetTicksX(thread_t *tp);
+stkalign_t* chThdGetWorkingAreaX(thread_t *tp);
+bool chThdTerminatedX(thread_t *tp);
+bool chThdShouldTerminateX();
+thread_t* chThdStartI(thread_t *tp);
+void chThdSleepS(sysinterval_t ticks);
+void chThdQueueObjectInit(threads_queue_t *tqp);
+bool chThdQueueIsEmptyI(threads_queue_t *tqp);
+void chThdDoDequeueNextI(threads_queue_t *tqp, msg_t msg);
--- source/chibid/rt/chtime.d
+++ source/chibid/rt/chtime.d
@@ -10,7 +10,15 @@ enum sysinterval_t TIME_INFINITE = cast(sysinterval_t)-1;
 enum sysinterval_t TIME_MAX_INTERVAL = cast(sysinterval_t)-2;
 enum sysinterval_t TIME_MAX_SYSTIME = cast(systime_t)-1;
 
-// TODO complete macros
+sysinterval_t TIME_S2I(time_secs_t secs) { return cast(time_conv_t)secs * cast(time_conv_t)CH_CFG_ST_FREQUENCY; }
+sysinterval_t TIME_MS2I(time_msecs_t msecs) { return (cast(time_conv_t)msecs * cast(time_conv_t)CH_CFG_ST_FREQUENCY + 999) / 1000;}
+sysinterval_t TIME_US2I(time_usecs_t usecs) { return (cast(time_conv_t)usecs * cast(time_conv_t)CH_CFG_ST_FREQUENCY + 999999) / 1000000;}
+time_secs_t TIME_I2S(sysinterval_t interval) { 
+    return cast(time_secs_t)((cast(time_conv_t)(interval) + cast(time_conv_t)CH_CFG_ST_FREQUENCY - cast(time_conv_t)1) / cast(time_conv_t)CH_CFG_ST_FREQUENCY); }
+time_msecs_t TIME_I2MS(sysinterval_t interval) {
+        return cast(time_secs_t)((cast(time_conv_t)(interval) * 1000 + cast(time_conv_t)CH_CFG_ST_FREQUENCY - cast(time_conv_t)1) / cast(time_conv_t)CH_CFG_ST_FREQUENCY); } 
+time_usecs_t TIME_I2US(sysinterval_t interval) {
+        return cast(time_secs_t)((cast(time_conv_t)(interval) * 1000000 + cast(time_conv_t)CH_CFG_ST_FREQUENCY - cast(time_conv_t)1) / cast(time_conv_t)CH_CFG_ST_FREQUENCY); }
 
 alias systime_t = ulong;
 alias sysinterval_t = ulong;
